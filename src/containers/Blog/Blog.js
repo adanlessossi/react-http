5e1ad04570d4f7047111f26e534e55d5bffabc02
adanlessossi <adanlessossi@hotmail.com>
2019-03-06 09:54:02 +0100

    Added axios interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+	state = {
+		posts: [],
+		selectedPostId: null,
+		error: false
+	};
+	componentDidMount() {
+		axios
+			.get('/posts')
+			.then(response => {
+				const posts = response.data.slice(0, 4);
+				const updatedPosts = posts.map(post => {
+					return {
+						...post,
+						author: 'Ben'
+					};
+				});
+				this.setState({ posts: updatedPosts });
+			})
+			.catch(error => {
+				this.setState({ error: true });
+			});
+	}
+	postSelectedHandler = id => {
+		this.setState({ selectedPostId: id });
+	};
+
+	render() {
+		let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+		if (!this.state.error) {
+			// @ts-ignore
+			posts = this.state.posts.map(post => {
+				return (
+					<Post
+						clicked={() => this.postSelectedHandler(post.id)}
+						key={post.id}
+						title={post.title}
+						author={post.author}
+					/>
+				);
+			});
+		}
+		return (
+			<div>
+				<section className='Posts'>{posts}</section>
+				<section>
+					<FullPost id={this.state.selectedPostId} />
+				</section>
+				<section>
+					<NewPost />
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Blog;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
+// @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(
+	request => {
+		console.log(request);
+		return request;
+	},
+	error => {
+		console.log(error);
+		return Promise.reject(error);
+	}
+);
+
+axios.interceptors.response.use(
+	response => {
+		console.log(response);
+		return response;
+	},
+	error => {
+		console.log(error);
+		return Promise.reject(error);
+	}
+);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
